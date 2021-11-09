8e3c5b2f8fbd23a372312e9d7d8b023063e93e34
Anthony <anthonyvillegas@berkeley.edu>
2021-11-08 16:23:58 -0800

    recieve and transmit now working

--- nrf_radio_transmit/nrf_radio_transmit.ino
+++ nrf_radio_transmit/nrf_radio_transmit.ino
@@ -1,34 +1,18 @@
 //This code allows us to TRANSMIT using the nrf24 module
 
-//Include Libraries
-//Libraries can be downladed here: https://github.com/nRF24/RF24
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
-
-//create an RF24 object
-//42 and 40 are the digital pins we connect to on the mega
-RF24 radio(42, 40);  // CE, CSN
-
-//address through which two modules communicate.
+RF24 radio(7, 8); // CE, CSN
 const byte address[6] = "00001";
-
-void setup()
-{
+void setup() {
   radio.begin();
-  
-  //set the address
   radio.openWritingPipe(address);
-  
-  //Set module as transmitter
+  radio.setPALevel(RF24_PA_MIN);
   radio.stopListening();
 }
-void loop()
-{
-  //Send message to receiver
+void loop() {
   const char text[] = "Hello World";
-  //returns boolean on whether message recieved, should delay launch sequence if message not recieved
   radio.write(&text, sizeof(text));
-  
   delay(1000);
 }
